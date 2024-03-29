f313515df1f3dabb368993f44f9d1d8ba22b0494
kalyani <Kalyani22@navgurukul.org>
2024-03-29 12:07:57 +0530

    rtdb is done

--- app/src/main/java/com/example/quizzicalwonders/MainActivity.kt
+++ app/src/main/java/com/example/quizzicalwonders/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.quizzicalwonders
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btnTextvicePractice:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnTextvicePractice=findViewById(R.id.btnPractice)
+
+        btnTextvicePractice.setOnClickListener {
+            startActivity(Intent(this,Quizz::class.java))
+        }
     }
 }
